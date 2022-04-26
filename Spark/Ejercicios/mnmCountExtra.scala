de7559050dc3310ad9c698e3e23fd61bb6e16312
Schobzax <blogete0@gmail.com>
2022-04-26 17:05:51 +0200

    Ejercicios extra capítulo 2 (por limpiar)

--- Spark/Ejercicios/mnmCountExtra.scala
+++ Spark/Ejercicios/mnmCountExtra.scala
@@ -22,17 +22,48 @@ object MnMCountExtra {
             .option("inferSchema","true")
             .load(mnmFile)
 
-        val countMnMDF = mnmDF 
+        val countMnMDF = mnmDF
+            .select("State","Color","Count")
+            .groupBy("State","Color")
+            .agg(count("Count").alias("Total"))
+            .orderBy(desc("Total"))
+
+        countMnMDF.show(60)
+        println(s"Total Rows = ${countMnMDF.count()}")
+        println()
+
+        val caCountMnMDF = mnmDF
+            .select("State","Color","Count")
+            .where(col("State") === "CA")
+            .groupBy("State","Color")
+            .agg(count("Count").alias("Total"))
+            .orderBy(desc("Total"))
+
+        caCountMnMDF.show(10)
+        println(s"Total Rows = ${countMnMDF.count()}")
+        println()
+
+        val maxMnMDF = mnmDF 
             .select("State","Color","Count")
             .groupBy("State","Color")
             .agg(max("Count").alias("Max"))
             .orderBy(col("Max").desc)
     
-        countMnMDF.show(60)
-        println(s"Total Rows = ${countMnMDF.count()}")
+        maxMnMDF.show(60)
+        println(s"Total Rows = ${maxMnMDF.count()}")
+        println()
+
+        val variosCountMnMDF = mnmDF
+            .select("State","Color","Count")
+            .where(col("State").isin("CA","TX","NV","CO"))
+            .groupBy("State","Color")
+            .agg(count("Count").alias("Total"))
+            .orderBy(desc("Total"))
+
+        variosCountMnMDF.show(60)
+        println(s"Total Rows = ${variosCountMnMDF.count()}")
         println()
 
         spark.stop()
-
     }
 }
--- Spark/Ejercicios/mnmcountExtra.py
+++ Spark/Ejercicios/mnmcountExtra.py
@@ -22,8 +22,37 @@ if __name__ == "__main__":
     count_mnm_df = (mnm_df
         .select("State","Color","Count")
         .groupBy("State","Color")
+        .agg(count("Count").alias("Total"))
+        .orderBy("Total", ascending=False))
+    
+    count_mnm_df.show(n=60, truncate=False)
+    print("Total Rows = %d" % (count_mnm_df.count()))
+
+    count_mnm_df = (mnm_df
+        .select("State","Color","Count")
+        .where(mnm_df.State == "CA")
+        .groupBy("State","Color")
+        .agg(count("Count").alias("Total"))
+        .orderBy("Total", ascending=False))
+
+    count_mnm_df.show(n=60, truncate=False)
+    print("Total Rows = %d" % (count_mnm_df.count()))
+
+    count_mnm_df = (mnm_df
+        .select("State","Color","Count")
+        .groupBy("State","Color")
         .agg(max("Count").alias("Max"))
         .orderBy("Max",ascending=True))
+
+    count_mnm_df.show(n=60, truncate=False)
+    print("Total Rows = %d" % (count_mnm_df.count()))
+
+    count_mnm_df = (mnm_df
+        .select("State","Color","Count")
+        .where(mnm_df.State.isin("CA","TX","NV","CO"))
+        .groupBy("State","Color")
+        .agg(count("Count").alias("Total"))
+        .orderBy("Total", ascending=False))
 
     count_mnm_df.show(n=60, truncate=False)
     print("Total Rows = %d" % (count_mnm_df.count()))
