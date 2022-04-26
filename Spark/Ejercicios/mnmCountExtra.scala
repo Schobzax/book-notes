64c4c59a394d2ab652deafaabbe679bd1de0e3e6
Schobzax <blogete0@gmail.com>
2022-04-26 17:13:01 +0200

    Comentarios en los ejercicios M&M

--- Spark/Ejercicios/mnmCountExtra.scala
+++ Spark/Ejercicios/mnmCountExtra.scala
@@ -1,26 +1,32 @@
 package main.scala.chapter2.extra.mnm
 
+// Imports
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
 object MnMCountExtra {
     def main(args: Array[String]) {
+        // Construimos la SparkSession
         val spark = SparkSession
             .builder
             .appName("MnMCountExtra")
             .getOrCreate()
         
+        // Comprobamos el número de argumentos
         if (args.length > 1) {
             print("Usage: MnMCount <mnm_file_dataset>")
             sys.exit(1)
         }
 
-        val mnmFile = args(0) // O la ruta
+        val mnmFile = args(0)
 
+        // Creamos el DataFrame
         val mnmDF = spark.read.format("csv")
             .option("header","true")
             .option("inferSchema","true")
             .load(mnmFile)
+
+        // Conteo de todos los M&Ms agrupados por Estado y Color
 
         val countMnMDF = mnmDF
             .select("State","Color","Count")
@@ -32,6 +38,9 @@ object MnMCountExtra {
         println(s"Total Rows = ${countMnMDF.count()}")
         println()
 
+        // Conteo de todos los M&Ms agrupados por Estado y Color
+        // Siendo el Estado = California
+        
         val caCountMnMDF = mnmDF
             .select("State","Color","Count")
             .where(col("State") === "CA")
@@ -43,6 +52,8 @@ object MnMCountExtra {
         println(s"Total Rows = ${countMnMDF.count()}")
         println()
 
+        // Nº máximo de M&Ms por Estado y Color
+
         val maxMnMDF = mnmDF 
             .select("State","Color","Count")
             .groupBy("State","Color")
@@ -53,6 +64,9 @@ object MnMCountExtra {
         println(s"Total Rows = ${maxMnMDF.count()}")
         println()
 
+        // Conteo de M&Ms por Estado y Color
+        // Siendo Estado = California, Texas, Nevada o Colorado
+        
         val variosCountMnMDF = mnmDF
             .select("State","Color","Count")
             .where(col("State").isin("CA","TX","NV","CO"))
@@ -65,5 +79,9 @@ object MnMCountExtra {
         println()
 
         spark.stop()
+
+    // Para cada ejercicio realizamos la consulta correspondiente
+    // Mostramos las filas necesarias
+    // E imprimimos el número de filas
     }
 }
--- Spark/Ejercicios/mnmcountExtra.py
+++ Spark/Ejercicios/mnmcountExtra.py
@@ -1,23 +1,29 @@
+# Imports
 import sys
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import count, max, min
 
 if __name__ == "__main__":
+    # Comprobamos el número de argumentos
     if len(sys.argv) != 2:
             print("Usage: mnmcount <file>", file=sys.stderr)
             sys.exit(-1)
 
+    # Construimos una SparkSession
     spark = (SparkSession
         .builder
         .appName("PythonMnMCountExtra")
         .getOrCreate())
 
+    # Leemos y creamos el DataFrame
     mnm_file = sys.argv[1]
     mnm_df = (spark.read.format("csv")
         .option("header","true")
         .option("inferSchema","true")
         .load(mnm_file))
+
+    # Conteo de todos los M&Ms agrupados por Estado y Color
 
     count_mnm_df = (mnm_df
         .select("State","Color","Count")
@@ -27,6 +33,9 @@ if __name__ == "__main__":
     
     count_mnm_df.show(n=60, truncate=False)
     print("Total Rows = %d" % (count_mnm_df.count()))
+
+    # Conteo de todos los M&Ms agrupados por Estado y Color
+    # Siendo el Estado = California
 
     count_mnm_df = (mnm_df
         .select("State","Color","Count")
@@ -38,6 +47,8 @@ if __name__ == "__main__":
     count_mnm_df.show(n=60, truncate=False)
     print("Total Rows = %d" % (count_mnm_df.count()))
 
+    # Nº máximo de M&Ms por Estado y Color
+
     count_mnm_df = (mnm_df
         .select("State","Color","Count")
         .groupBy("State","Color")
@@ -47,6 +58,9 @@ if __name__ == "__main__":
     count_mnm_df.show(n=60, truncate=False)
     print("Total Rows = %d" % (count_mnm_df.count()))
 
+    # Conteo de M&Ms por Estado y Color
+    # Siendo Estado = California, Texas, Nevada o Colorado
+    
     count_mnm_df = (mnm_df
         .select("State","Color","Count")
         .where(mnm_df.State.isin("CA","TX","NV","CO"))
@@ -58,3 +72,7 @@ if __name__ == "__main__":
     print("Total Rows = %d" % (count_mnm_df.count()))
 
     spark.stop()
+
+# Para cada ejercicio realizamos la consulta correspondiente
+# Mostramos las filas necesarias
+# E imprimimos el número de filas
